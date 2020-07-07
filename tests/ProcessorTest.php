efd2db5838468f5fdb05bc26d14b71a280347cba
Devang Mehta <devang@jdmehta.co.uk>
2020-07-07 06:59:22 +0100

    improved tests

--- tests/ProcessorTest.php
+++ tests/ProcessorTest.php
@@ -4,15 +4,17 @@ use PHPUnit\Framework\TestCase;
 
 class ProcessorTest extends TestCase
 {
-    public function testResizeIfNeeded()
+    public function testResizeIfNeededWithException()
     {
         try {
             Processor::resizeIfNeeded('/app/tests/txtfile.txt');
-        }
-        catch (Exception $ex)
-        {
+        } catch (Exception $ex) {
             $this->assertInstanceOf(GetImageSizeException::class, $ex);
         }
+    }
+
+    public function testResizeIfNeededNotNeeded()
+    {
 
         // file not to be resized
         $resized = Processor::resizeIfNeeded('/app/tests/small-image.jpg', true);
@@ -21,7 +23,10 @@ class ProcessorTest extends TestCase
         // wide image to be resized
         $resized = Processor::resizeIfNeeded('/app/tests/wide-image.jpg', true);
         $this->assertTrue($resized);
+    }
 
+    public function testResizeIfNeededNeeded()
+    {
         // tall image to be resized
         $resized = Processor::resizeIfNeeded('/app/tests/tall-image.jpg', true);
         $this->assertTrue($resized);
