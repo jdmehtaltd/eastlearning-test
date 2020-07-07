581d3c327ddb636ddc65675abcc98371c8cf9e05
Devang Mehta <devang@jdmehta.co.uk>
2020-07-07 07:00:56 +0100

    improved tests (#6)

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -5,18 +5,21 @@ use PHPUnit\Framework\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    public function testCheckIfValidImageFormat(): void
+    public function testCheckIfValidImageFormatNoException(): void
     {
-        try
-        {
+        try {
             Validator::checkIfValidImageFormat('abc.jpg');
             Validator::checkIfValidImageFormat('abc.jpeg');
-        }
-        catch(NotAllowedImageFormatException $ex)
-        {
+        } catch (NotAllowedImageFormatException $ex) {
             # TODO: simulating test failure, look for something like fail() method
             $this->assertTrue(null);
         }
+        # TODO: test success, is this the right way?
+        $this->assertTrue(true);
+    }
+
+    public function testCheckIfValidImageFormatWithException(): void
+    {
         try
         {
             Validator::checkIfValidImageFormat('abc.pdf');
@@ -27,17 +30,20 @@ class ValidatorTest extends TestCase
         }
     }
 
-    public function testCheckMaxSize(): void
+    public function testCheckMaxSizeNoException(): void
     {
-        try
-        {
+        try {
             Validator::checkMaxSize(1); // 1 byte
-        }
-        catch(MaxFileSizeExceededException $ex)
-        {
+        } catch (MaxFileSizeExceededException $ex) {
             # TODO: simulating test failure, look for something like fail() method
             $this->assertTrue(null);
         }
+        # TODO: test success, is this the right way?
+        $this->assertTrue(true);
+    }
+
+    public function testCheckMaxSizeWithException(): void
+    {
         try
         {
             Validator::checkMaxSize(1000000000); // 1GB
@@ -47,18 +53,21 @@ class ValidatorTest extends TestCase
         }
     }
 
-    public function testCheckIfRealImage(): void
+    public function testCheckIfRealImageNoException(): void
     {
-        try
-        {
+        try {
             Validator::checkIfRealImage('/app/tests/JPEG_compression_Example.jpg', 'image/jpeg');
-        }
-        catch(NotRealImageException $ex)
-        {
+        } catch (NotRealImageException $ex) {
             # TODO: simulating test failure, look for something like fail() method
             $this->assertTrue(null);
             return;
         }
+        # TODO: test success, is this the right way?
+        $this->assertTrue(true);
+    }
+
+    public function testCheckIfRealImageWithException(): void
+    {
         try {
             Validator::checkIfRealImage('/app/tests/txtfile.txt', 'text/plain');
         }
